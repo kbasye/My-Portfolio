6cc33645df71fb8eb633569b59dbdc470835044d
kbasye <kbasye@gmail.com>
2013-09-04 18:57:59 -0700

    Cleaning up Sessions Controller and Login Page.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,13 @@ class SessionsController < ActionController::Base
 	end
 	
 	def create
-		@user = User.find_by_username(params[:username])
-		if @user.authenticate(params[:password])
-			session[:auth_token] = @user.auth_token
-			redirect_to root_url
+		if User.exists?(:username => params[:username])
+			@user = User.find_by_username(params[:username])
+			if @user.authenticate(params[:password])
+				session[:auth_token] = @user.auth_token
+				redirect_to root_url
+			end
 		else
-			flash.now.alert = "Invalid email or password"
 			render 'new'
 		end
 	end
